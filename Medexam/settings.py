596a25dcf5504fd1d6062031dd51e4c66d130218
victortoromanuel <victortoromanuel@gmail.com>
2022-07-17 15:33:53 -0500

    Cambios para el deploy

--- Medexam/settings.py
+++ Medexam/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-&(5jymhwtpr&n*g@8v8nf*#$vd_9uyi+o@_%z=bf^*)5c&g(9#
 DEBUG = True
 
 #ALLOWED_HOSTS = ['*']
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['134.122.125.243']
 
 # Application definition
 
@@ -146,9 +146,13 @@ CORS_ORIGIN_ALLOW_ALL = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
-
+"""
 STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
+"""
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
